57eb1e3d81cc03c2020fb3cb1f3637dd31bf8f3f
Krzysztof <twoj_email@example.com>
2026-01-11 12:48:21 +0100

    poprawka

--- src/pages/favorites.jsx
+++ src/pages/favorites.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import weatherData from "../data/weatherData";
 
-{/*dodaje miasta do ulub */}
+
 export default function Favorites() {
   const favorites = useSelector((state) => state.favorites.cities);
   const unit = useSelector((state) => state.temperature.unit);
